dfd69aad49079356c5bc9210ceb7f9ebdaed5e42
telecom <telecom@C059654.pucp.edu.pe>
2016-02-23 15:47:56 -0500

    esqueleto de la aplicación

--- index.php
+++ index.php
@@ -1,17 +1,64 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+
+        <!-- Optional theme -->
+        <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous"-->
+        <link rel="stylesheet" href="assets/css/jumbotron-narrow.css" />
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="assets/js/jquery-2.2.1.min.js"></script>   
+        <script src="assets/js/jquery-ui.min.js"></script>             
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script>
+
+        </script>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="container">
+            <div class="header clearfix">
+                <h3 class="text-muted">Grupo #: Los electrónicos</h3>
+            </div>
+
+            <div class="jumbotron">
+                <h1>Jumbotron heading</h1>
+                <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+            </div>
+
+            <div class="row marketing">
+                <div class="col-lg-6">
+                    <h4>Subheading</h4>
+                    <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+                    <h4>Subheading</h4>
+                    <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+                    <h4>Subheading</h4>
+                    <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+                </div>
+
+                <div class="col-lg-6">
+                    <h4>Subheading</h4>
+                    <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+                    <h4>Subheading</h4>
+                    <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+                    <h4>Subheading</h4>
+                    <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+                </div>
+            </div>
+
+            <footer class="footer">
+                <p>© 2015 Company, Inc.</p>
+            </footer>
+
+        </div> <!-- /container -->
+
     </body>
 </html>
