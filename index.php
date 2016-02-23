3761279eba3d4c8d556b3574a53c05d4e3fb5aae
telecom <telecom@C059654.pucp.edu.pe>
2016-02-23 16:36:09 -0500

    esqueleto de la aplicación

--- index.php
+++ index.php
@@ -25,37 +25,19 @@
             </div>
 
             <div class="jumbotron">
+                <table class="table table-hover">
+                    <tr>
+                        <th></th>
+                    </tr>
+                </table>
+
                 <h1>Jumbotron heading</h1>
                 <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
                 <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
             </div>
 
-            <div class="row marketing">
-                <div class="col-lg-6">
-                    <h4>Subheading</h4>
-                    <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-                    <h4>Subheading</h4>
-                    <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-                    <h4>Subheading</h4>
-                    <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-                </div>
-
-                <div class="col-lg-6">
-                    <h4>Subheading</h4>
-                    <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-                    <h4>Subheading</h4>
-                    <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-                    <h4>Subheading</h4>
-                    <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-                </div>
-            </div>
-
             <footer class="footer">
-                <p>© 2015 Company, Inc.</p>
+                <p>© 2016 - Proyecto introducción a la Ing. Electrónica y las Telecomunicaciones</p>
             </footer>
 
         </div> <!-- /container -->
